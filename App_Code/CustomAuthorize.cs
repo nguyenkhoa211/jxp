a7498adae50ccabcdd31c1c89c50994d2b9c6f00
MickeyBoo <mickey.boo.0112@gmail.com>
2017-03-26 20:54:46 +0700

    add client portal Page, UI

---
+++ App_Code/CustomAuthorize.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+/// <summary>
+/// Summary description for CustomAuthorize
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+public class CustomAuthorize : AuthorizeAttribute
+{
+    public override void OnAuthorization(AuthorizationContext filterContext)
+    {
+
+        try
+        {
+            if (SessionManager.UserLogin == null)
+            {
+                filterContext.Result = new RedirectResult("/login");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new HttpException(403, "Unauthorization");
+        }
+    }
+}
---
+++ App_Code/CustomUmbracoApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Umbraco.Core;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+/// <summary>
+/// Summary description for CustomUmbracoApplication
+/// </summary>
+public class CustomUmbracoApplication : UmbracoApplication
+{
+    protected override IBootManager GetBootManager()
+    {
+        return new CustomWebBootManager(this);
+    }
+}
+class CustomWebBootManager : WebBootManager
+{
+    public CustomWebBootManager(Umbraco.Core.UmbracoApplicationBase umbracoApplication) : base(umbracoApplication)
+    {
+    }
+
+    public override Umbraco.Core.IBootManager Complete(Action<ApplicationContext> afterComplete)
+    {
+        RouteTable.Routes.MapUmbracoRoute("clientPortal", "userproducts/{action}/{id}",
+            new { controller = "Products", id = UrlParameter.Optional }, new UmbracoVirtualNodeByIdRouteHandler(1373));
+        return base.Complete(afterComplete);
+    }
+}
